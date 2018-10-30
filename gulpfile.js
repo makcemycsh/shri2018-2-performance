589a4d60f25a92c848a283af47b625c8b2bdc902
Шилов Максим <m.shilo.v80@gmail.com>
2018-10-30 10:59:18 +0300

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,49 @@
-const gulp        = require('gulp');
-const watch       = require('gulp-watch'); // Более умный вотчер
-const batch       = require('gulp-batch'); // Пачки задач
-const plumber     = require('gulp-plumber'); // Обработка ошибок
-const notify      = require('gulp-notify');
+const gulp = require('gulp');
+const watch = require('gulp-watch'); // Более умный вотчер
+const batch = require('gulp-batch'); // Пачки задач
+const plumber = require('gulp-plumber'); // Обработка ошибок
+const notify = require('gulp-notify');
 const browserSync = require('browser-sync').create();
-const exec        = require('gulp-exec'); // Выполнение команд
-const minimist    = require('minimist'); // Работа с аргументами команд
-
+const exec = require('gulp-exec'); // Выполнение команд
+const minimist = require('minimist'); // Работа с аргументами команд
 //
-const twig = require('gulp-twig');
-//
-const sass = require('gulp-sass');
 
-const sassGlob     = require('gulp-sass-glob');
-const postcss      = require('gulp-postcss');
+const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
-const cssnano      = require('cssnano');
-const sourcemaps   = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
 //
-const babel        = require('gulp-babel');
-const uglify       = require('gulp-uglify');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 //
-const imagemin     = require('gulp-imagemin');
-const prettify     = require('gulp-html-prettify');
+const imagemin = require('gulp-imagemin');
+const prettify = require('gulp-html-prettify');
 
 
 // Обработка ошибок
 const handleError = (err) => {
   notify.onError({
-    title:   'Gulp error',
+    title: 'Gulp error',
     message: err.message,
   })(err);
 };
 
 // Обработка аргументов
 const knownOptions = {
-  string:  'env',
+  string: 'env',
   default: {env: process.env.NODE_ENV || 'production'},
 };
 
 const options = minimist(process.argv.slice(2), knownOptions);
 
-// 1. Девсервер на build/
+// 1. Девсервер на docs/
 gulp.task('server', () => {
   browserSync.init({
     server: {
-      baseDir: 'build/',
+      baseDir: 'docs/',
     },
-    host:   'localhost',
-    port:   9000,
+    host: 'localhost',
+    port: 9000,
     notify: false,
   });
 });
@@ -56,125 +51,86 @@ gulp.task('server:refresh', () => {
   browserSync.reload();
 });
 gulp.task('server:inject', () => {
-  gulp.src('build/styles/**/*.*')
-      .pipe(browserSync.stream());
+  gulp.src('docs/styles/**/*.*').pipe(browserSync.stream());
 });
 
 // 2. Билды
-gulp.task('build:html', () => {
+gulp.task('docs:html', () => {
   gulp.src([
-    'src/pages/*.twig',
     'src/pages/*.html',
-  ])
-      .pipe(twig())
-      .pipe(prettify({
-        indent_char:           ' ',
-        indent_size:           2,
-        preserve_newlines:     true,
-        max_preserve_newlines: 1,
-        end_with_newline:      true,
-        wrap_line_length:      80,
-      }))
-      .pipe(gulp.dest('build/'));
+  ]).pipe(prettify({
+    indent_char: ' ',
+    indent_size: 2,
+    preserve_newlines: true,
+    max_preserve_newlines: 1,
+    end_with_newline: true,
+    wrap_line_length: 80,
+  })).pipe(gulp.dest('docs/'));
 });
-gulp.task('build:styles', () => {
-  gulp.src('src/styles/*.scss')
-  // .pipe(sourcemaps.init())
-      .pipe(plumber(handleError))
-      .pipe(sassGlob())
-      .pipe(sass())
-      .pipe(postcss([
-        autoprefixer({
-          browsers: ['last 2 version'],
-          grid:     true,
-        }),
-        cssnano({
-          zindex:       false,
-          reduceIdents: false,
-        }),
-      ]))
-  // .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('build/styles/'));
+gulp.task('docs:styles', () => {
+  gulp.src('src/styles/*.css').pipe(plumber(handleError)).pipe(postcss([
+    cssnano({
+      zindex: false,
+      reduceIdents: false,
+    }),
+  ])).pipe(gulp.dest('docs/styles/'));
 });
-gulp.task('build:scripts', () => {
-  gulp.src('src/scripts/vendor/*.js')
-      // .pipe(uglify())
-      .pipe(gulp.dest('build/scripts/vendor/'));
+gulp.task('docs:scripts', () => {
 
-  gulp.src('src/scripts/*.js')
-      .pipe(sourcemaps.init())
-      .pipe(plumber(handleError))
-      .pipe(babel({
-        presets: ['env'],
-      }))
-      .pipe(uglify())
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('build/scripts/'));
+  gulp.src('src/scripts/*.js').pipe(plumber(handleError)).pipe(babel({
+    presets: ['env'],
+  })).pipe(uglify()).pipe(gulp.dest('docs/scripts/'));
 });
-gulp.task('build:assets', () => {
-  gulp.src('src/assets/fonts/**/*.*')
-      .pipe(gulp.dest('build/assets/fonts/'));
+gulp.task('docs:assets', () => {
+  gulp.src('src/fonts/**/*.*').pipe(gulp.dest('docs/fonts/'));
 
-  gulp.src('src/assets/other/**/*.*')
-      .pipe(gulp.dest('build/assets/other/'));
-
-  gulp.src('src/assets/img/**/*.*')
-      .pipe(imagemin())
-      .pipe(gulp.dest('build/assets/img/'));
-
-  gulp.src('src/assets/favicon/**/*.*')
-      .pipe(gulp.dest('build/assets/favicon/'));
+  gulp.src('src/assets/img/**/*.*').pipe(imagemin()).pipe(gulp.dest('docs/assets/img/'));
 });
 
 // 3. Вотчеры
-gulp.task('watch:build', ['server', 'build:html', 'build:styles', 'build:scripts', 'build:assets'], () => {
+gulp.task('watch:docs', ['server', 'docs:html', 'docs:styles', 'docs:scripts', 'docs:assets'], () => {
   watch([
-    'src/pages/**/*.twig',
     'src/pages/**/*.html',
-    'src/blocks/**/*.twig',
     'src/blocks/**/*.html',
   ], batch((e, end) => {
-    gulp.start('build:html', end);
+    gulp.start('docs:html', end);
   }));
 
   watch([
     'src/styles/**/*.*',
-    'src/blocks/**/*.scss',
   ], batch((e, end) => {
-    gulp.start('build:styles', end);
+    gulp.start('docs:styles', end);
   }));
 
 
   watch('src/scripts/**/*.*', batch((e, end) => {
-    gulp.start('build:scripts', end);
+    gulp.start('docs:scripts', end);
   }));
   watch('src/assets/**/*.*', batch((e, end) => {
-    gulp.start('build:assets', end);
+    gulp.start('docs:assets', end);
   }));
 });
 gulp.task('watch:update', () => {
   watch([
-    'build/*.html',
-    'build/scripts/**/*.*',
-    'build/assets/**/*.*',
+    'docs/*.html',
+    'docs/scripts/**/*.*',
+    'docs/assets/**/*.*',
   ], batch((e, end) => {
     gulp.start('server:refresh', end);
   }));
 
-  watch('build/styles/**/*.css', batch((e, end) => {
+  watch('docs/styles/**/*.css', batch((e, end) => {
     gulp.start('server:inject', end);
   }));
 });
 
 // 3. Shell-задачи
 gulp.task('createBlock', () => {
-  gulp.src('')
-      .pipe(exec(`sh tasks/createBlock.sh ${options.name}`));
+  gulp.src('').pipe(exec(`sh tasks/createBlock.sh ${options.name}`));
 });
 gulp.task('clearBuild', () => {
-  gulp.src('')
-      .pipe(exec('rm -rf build/'));
+  gulp.src('').pipe(exec('rm -rf docs/'));
 });
 
-gulp.task('default', ['watch:build', 'watch:update']);
-gulp.task('build', ['build:html', 'build:styles', 'build:scripts', 'build:assets']);
+gulp.task('default', ['watch:docs', 'watch:update']);
+gulp.task('docs', ['docs:html', 'docs:styles', 'docs:scripts', 'docs:assets']);
